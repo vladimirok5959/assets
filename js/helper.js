3c8ce9d6839c739c809c4d91ceeebe1c51143495
Volodymyr Tkach <vladimirok5959@gmail.com>
2022-07-30 03:54:08 +0300

    Optimize

--- js/helper.js
+++ js/helper.js
@@ -1,6 +1,11 @@
 var helper = {};
+
+helper.createDiv = function() {
+	return document.createElement('div');
+};
+
 helper.toHtmlObject = function(htmlCode) {
-	var div = document.createElement('div');
+	var div = helper.createDiv();
 	div.innerHTML = htmlCode;
 	return div.firstChild;
 };
@@ -21,14 +26,14 @@ helper.hasClass = function(obj, className) {
 };
 
 helper.getScrollWidth = function() {
-	var outer = document.createElement('div');
+	var outer = helper.createDiv();
 	outer.style.visibility = 'hidden';
 	outer.style.width = '100px';
 	outer.style.msOverflowStyle = 'scrollbar';
 	document.body.appendChild(outer);
 	var widthNoScroll = outer.offsetWidth;
 	outer.style.overflow = 'scroll';
-	var inner = document.createElement('div');
+	var inner = helper.createDiv();
 	inner.style.width = '100%';
 	outer.appendChild(inner);
 	var widthWithScroll = inner.offsetWidth;
